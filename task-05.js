d983c3d455bd976ede487552a4736eb3c2e64588
Vito <topvetal@gmail.com>
2021-01-10 23:07:47 +0200

    task-05

---
+++ task-05.js
@@ -0,0 +1,11 @@
+//Задание 5
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку. Функция проверяет ее на содержание слов spam и sale. Если нашли зарещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false. Слова в строке могут быть в произвольном регистре.
+
+const checkForSpam = (message) => message.toLowerCase().includes('sale') || message.toLowerCase().includes('spam');
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
