768d9a9797e48ad98eaaeaf027a70e12ac5a6736
Vito <topvetal@gmail.com>
2021-01-10 22:36:14 +0200

    task-03

---
+++ task-03.js
@@ -0,0 +1,31 @@
+//Задание 3
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+// const findLongestWord = function(string) {
+//   // твой код
+// };
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+// console.log(findLongestWord('Google do a roll')); // 'Google'
+// console.log(findLongestWord('May the force be with you')); // 'force'
+
+const findLongestWord = (string) => {
+  let
+    longest = 0,
+    longestWord
+  for (let word of string.split(' ')) {
+    if (word.length >= longest) {
+      longest = word.length
+      longestWord = word
+    }
+  }
+  return (longestWord)
+};
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+console.log(findLongestWord('Google do a roll')); // 'Google'
+console.log(findLongestWord('May the force be with you')); // 'force'
+
